0e654ceec1e2e802ef9a3537d8a9e8d6f8dcec15
Caleb Leung <caleb.leungg@gmail.com>
2020-05-30 16:09:20 +1000

    filtering of portfolio

--- styles/script.js
+++ styles/script.js
@@ -11,3 +11,38 @@ nightToggle.addEventListener('click', () => {
         toggleNight = 0
     }
 })
+
+let projects = document.getElementsByClassName('project-item')
+let options = document.getElementsByClassName('option-select')
+
+for (cat of options) {
+    let category = cat.innerHTML
+    let check = {
+        'HTML / CSS': 'static-html',
+        'RUBY / RAILS': 'ruby',
+        'JAVASCRIPT': 'javascript',
+        'MERN': 'mern'
+    }
+    cat.addEventListener('click', () => {
+        for (project of projects) {
+            project.setAttribute('id', 'invisible')
+        }
+        if (category === "ALL") {
+            for (project of projects) {
+                project.setAttribute('id', 'visible')
+                console.log(project)
+            }
+        } else {
+            for(item of Object.keys(check)) {
+                if (item === category) {
+                    let displayList = document.getElementsByClassName(check[item])
+                    for (project of displayList) {
+                        // project.setAttribute('class', 'option-select')
+                        project.setAttribute('id', 'visible')
+                    } 
+                } 
+            }
+        }
+    })
+}
+
